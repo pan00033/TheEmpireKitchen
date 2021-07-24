69c5ae83793f6b137c0a896f48c714301a5e3038
ArizonaPeachTea <zuxingp888@gmail.com>
2021-07-23 22:06:50 -0500

    fixing meal item form input IDs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -4,14 +4,15 @@ import classes from './MealItemForm.module.css';
 const MealItemForm = props => {
     return (
         <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amount',
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
+            <Input label="Amount"
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1'
+                }} />
             <button>+ Add</button>
         </form>
     );
